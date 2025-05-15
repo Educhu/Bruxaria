66f83a16a66331fb543eae8d39f61a4479faca49
Educhu <eduardociocci0205@gmail.com>
2025-05-14 21:40:43 -0300

    sprites e embaralhamento correto.

--- Assets/Scripts/Battle/BattleUIManager.cs
+++ Assets/Scripts/Battle/BattleUIManager.cs
@@ -1,15 +1,23 @@
 using UnityEngine;
-using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class BattleUIManager : MonoBehaviour
 {
-    public GameObject mainButtons;   // Painel dos botões principais (Ex: Escrever Feitiço, Usar Pergaminho)
-    public GameObject spellButtons;  // Painel dos botões do alfabeto para escrever feitiço
+    public GameObject mainButtons;
+    public GameObject spellButtons;
     public GameObject parchmentButtons;
     public GameObject LogText;
+    public GameObject alfaBeto; // Objeto que contém os botões A, B, C...
+
+    private void Start()
+    {
+        RandomizeLetterTexts();
+    }
 
     public void OpenSpellWriting()
     {
+        RandomizeLetterTexts();
+
         mainButtons.SetActive(false);
         spellButtons.SetActive(true);
         LogText.SetActive(false);
@@ -32,5 +40,24 @@ public class BattleUIManager : MonoBehaviour
     {
         parchmentButtons.SetActive(true);
         mainButtons.SetActive(false);
+    }
+
+    private void RandomizeLetterTexts()
+    {
+        // Para cada filho direto de ALFAabeto (A, B, C...)
+        for (int i = 0; i < alfaBeto.transform.childCount; i++)
+        {
+            Transform letraObj = alfaBeto.transform.GetChild(i);
+
+            // Procura o filho chamado "Text (TMP)" dentro de cada letra
+            Transform textoTransform = letraObj.Find("Text (TMP)");
+
+            if (textoTransform != null)
+            {
+                // Ativa ou desativa aleatoriamente o texto
+                bool mostrarTexto = Random.value > 0.5f;
+                textoTransform.gameObject.SetActive(mostrarTexto);
+            }
+        }
     }
 }
