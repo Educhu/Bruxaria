4237d9391135744ad67d5158eb38fc0a55685b8d
Educhu <eduardociocci0205@gmail.com>
2025-05-19 22:06:35 -0300

    EnemyData erro
    KKKKKKKKKKKKKKKKKKKKK

--- Assets/Scripts/Battle/Enemy.cs
+++ Assets/Scripts/Battle/Enemy.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Enemy : MonoBehaviour
 {
@@ -6,13 +7,41 @@ public class Enemy : MonoBehaviour
     public int maxHealth = 100;
     public int speed = 10;
     public Element element = Element.Fire;
-    private SpriteRenderer spriteRenderer;
 
-    public HealthBar healthBar; // Referência para a barra de vida
+    public HealthBar healthBar;
+
+    private Animator animator;
+
+    [Header("Animator Setup")]
+    public RuntimeAnimatorController baseAnimatorController;
+
+    [Header("Idle Clips")]
+    public AnimationClip fireIdle;
+    public AnimationClip waterIdle;
+    public AnimationClip earthIdle;
+    public AnimationClip airIdle;
+
+    [Header("Attack Clips")]
+    public AnimationClip fireAttack;
+    public AnimationClip waterAttack;
+    public AnimationClip earthAttack;
+    public AnimationClip airAttack;
+
+    [Header("Hit Clips")]
+    public AnimationClip fireHit;
+    public AnimationClip waterHit;
+    public AnimationClip earthHit;
+    public AnimationClip airHit;
+
+    [Header("Die Clips")]
+    public AnimationClip fireDie;
+    public AnimationClip waterDie;
+    public AnimationClip earthDie;
+    public AnimationClip airDie;
 
     private void Start()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>(); // Inicializa o SpriteRenderer
+        animator = GetComponent<Animator>();
         healthBar.SetMaxHealth(maxHealth);
 
         if (GameManager.Instance != null)
@@ -27,13 +56,100 @@ public class Enemy : MonoBehaviour
                 speed = data.speed;
                 element = data.element;
 
-                // Aplica a cor corretamente
-                //spriteRenderer.color = data.color;
+                ApplyElementAnimations(element);
 
                 healthBar.SetHealth(health);
-                Debug.Log("Inimigo carregado: " + gameObject.name + " | Cor: " + data.color);
+                Debug.Log("Inimigo carregado: " + gameObject.name + " | Elemento: " + data.element);
             }
         }
+    }
+
+    public void SetAttributes(EnemyData data)
+    {
+        health = data.health;
+        speed = data.speed;
+        element = data.element;
+
+        ApplyElementAnimations(element);
+    }
+
+    private void ApplyElementAnimations(Element e)
+    {
+        if (animator == null)
+            animator = GetComponent<Animator>();
+
+        var overrideController = new AnimatorOverrideController(baseAnimatorController);
+        var overrides = new List<KeyValuePair<AnimationClip, AnimationClip>>();
+
+        foreach (var clip in overrideController.animationClips)
+        {
+            switch (clip.name)
+            {
+                case "Idle":
+                    overrides.Add(new KeyValuePair<AnimationClip, AnimationClip>(clip, GetIdleClip(e)));
+                    break;
+                case "Attack":
+                    overrides.Add(new KeyValuePair<AnimationClip, AnimationClip>(clip, GetAttackClip(e)));
+                    break;
+                case "Hit":
+                    overrides.Add(new KeyValuePair<AnimationClip, AnimationClip>(clip, GetHitClip(e)));
+                    break;
+                case "Die":
+                    overrides.Add(new KeyValuePair<AnimationClip, AnimationClip>(clip, GetDieClip(e)));
+                    break;
+            }
+        }
+
+        overrideController.ApplyOverrides(overrides);
+        animator.runtimeAnimatorController = overrideController;
+    }
+
+    private AnimationClip GetIdleClip(Element e)
+    {
+        return e switch
+        {
+            Element.Fire => fireIdle,
+            Element.Water => waterIdle,
+            Element.Earth => earthIdle,
+            Element.Air => airIdle,
+            _ => fireIdle
+        };
+    }
+
+    private AnimationClip GetAttackClip(Element e)
+    {
+        return e switch
+        {
+            Element.Fire => fireAttack,
+            Element.Water => waterAttack,
+            Element.Earth => earthAttack,
+            Element.Air => airAttack,
+            _ => fireAttack
+        };
+    }
+
+    private AnimationClip GetHitClip(Element e)
+    {
+        return e switch
+        {
+            Element.Fire => fireHit,
+            Element.Water => waterHit,
+            Element.Earth => earthHit,
+            Element.Air => airHit,
+            _ => fireHit
+        };
+    }
+
+    private AnimationClip GetDieClip(Element e)
+    {
+        return e switch
+        {
+            Element.Fire => fireDie,
+            Element.Water => waterDie,
+            Element.Earth => earthDie,
+            Element.Air => airDie,
+            _ => fireDie
+        };
     }
 
     public ActionType ChooseAction(Player player)
@@ -41,15 +157,11 @@ public class Enemy : MonoBehaviour
         int rand = Random.Range(0, 100);
 
         if (health <= 30 && rand < 50)
-        {
-            return ActionType.Heal; // Se estiver com pouca vida, pode curar
-        }
+            return ActionType.Heal;
         else if (speed < player.speed && rand < 70)
-        {
-            return ActionType.SpeedBoost; // Se estiver mais lento, pode aumentar velocidade
-        }
+            return ActionType.SpeedBoost;
 
-        return ActionType.Attack; // Caso contrário, ataca
+        return ActionType.Attack;
     }
 
     public void TakeDamage(int amount)
@@ -66,17 +178,5 @@ public class Enemy : MonoBehaviour
         if (health > maxHealth) health = maxHealth;
         healthBar.SetHealth(health);
         Debug.Log("Inimigo curou " + amount + " de vida! Vida atual: " + health);
-    }
-
-    public void SetAttributes(EnemyData data)
-    {
-        health = data.health;
-        speed = data.speed;
-        element = data.element;
-
-        if (spriteRenderer == null)
-            spriteRenderer = GetComponent<SpriteRenderer>();
-
-        //spriteRenderer.color = data.color; // Aplica a cor corretamente
     }
 }
